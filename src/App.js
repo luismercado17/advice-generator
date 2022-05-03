8daa9a5614821965820d62b50338eba1fa84820a
luismercado17 <luismercado723@gmail.com>
2022-05-03 07:58:39 -0500

    Ultima versión

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import pattern from "./img/pattern-divider-desktop.svg";
+import diceicon from "./img/icon-dice.svg";
+import patternMobile from "./img/pattern-divider-mobile.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App =()=> {
+const [data, setdata] = useState();
+
+useEffect(() =>{
+  peticionApi ();
+},[])
+
+function peticionApi (){
+  fetch('https://api.adviceslip.com/advice/117')
+  .then(response => response.json())
+  .then(result => {
+    setdata(result.slip);
+    console.log(result);
+  })
+}
+
+console.log(data);
+
+return (
+  <div className="App">
+      <section>
+      {data?.id && ( 
+        <h4>Advice #{data.id}</h4>
+        )}
+        {data?.advice && ( 
+        <h2>"{data.advice}"</h2>
+        )}
+        <img src={pattern} className="border_pattern desktop_pattern"/>
+        <img src={patternMobile} className="border_pattern mobile_pattern"/>
+        <div className='row_icon'>
+        <img src={diceicon} className="icon_device"/>
+        </div>
+      </section>
     </div>
   );
 }
